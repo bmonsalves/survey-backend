82a82e56546b541211516d783d27649027176376
bmonsalves <bdo.monsalves@gmail.com>
2018-07-04 22:49:36 -0400

    lista y detalle de preguntas

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import { question } from './routes'
 
 const app = express();
 
-app.get('/',(req,res) => res.send("hola mundo"));
+if (process.env.NODE_ENV === 'development') {
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+        next()
+    })
+}
+
+app.use('/v1/questions', question);
 
 export default app;
