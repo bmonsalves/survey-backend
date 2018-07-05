d9d67a30c6e1d074887b26708db9c4d037f748bb
bmonsalves <bdo.monsalves@gmail.com>
2018-07-04 23:59:42 -0400

    crea preguntas

--- routes/question.js
+++ routes/question.js
@@ -19,10 +19,29 @@ const question = {
 
 const questions = new Array(10).fill(question);
 
-// /api/questions
+// /v1/questions
 app.get('/', (req, res) => res.status(200).json(questions));
 
 // /api/questions/:id
-app.get('/:id', (req, res) => res.status(200).json(question));
+app.get('/:id', (req, res) => {
+    const {id} = req.params;
+    const q = questions.find(({ _id }) => _id === +id);
+    res.status(200).json(q)
+});
 
+// /v1/questions/
+app.post('/', (req, res) => {
+    const q = req.body;
+    q._id = +new Date();
+    q.user = {
+        firstName: 'Bernardo',
+        lastName: 'Monsalves',
+        email: 'bmonsalves@monsalves.com',
+        password: '123456'
+    }
+    q.createdAt = new Date();
+    q.answers = [];
+    questions.push(q);
+    res.status(201).json(q)
+});
 export default app
